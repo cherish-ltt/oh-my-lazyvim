533b01f4ad46b224977a5e4f26c80ebccf2acd90
Ghyper9023 <ghyper9023@outlook.com>
2026-09-01 21:36:14 +0800

    202609012136

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    flavour = "latte",
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin-nvim")
+  end,
+}
